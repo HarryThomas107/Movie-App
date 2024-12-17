1d499f794d14cfc1caa74c8ec48d713b0332a0a4
HarryThomas107 <harrybinu@gmail.com>
2024-12-17 13:28:46 +0530

    Connected Login to Dashboard

--- app/src/main/java/com/example/movieapp/Dashboard.java
+++ app/src/main/java/com/example/movieapp/Dashboard.java
@@ -1,6 +1,9 @@
 package com.example.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +13,25 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Dashboard extends AppCompatActivity {
 
+    Button b2,b3,b4,b5;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
+
+        b2=(Button) findViewById(R.id.bt2);
+        b3=(Button) findViewById(R.id.bt3);
+        b4=(Button) findViewById(R.id.bt4);
+        b5=(Button) findViewById(R.id.bt5);
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ob2 = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(ob2);
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/movieapp/MainActivity.java
+++ app/src/main/java/com/example/movieapp/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.movieapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +15,35 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText e1,e2;
+    Button b1;
+    String getUser,getPass,s1="theatre",s2="123456",s3="Invalid Credentials";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        e1=(EditText) findViewById(R.id.et1);
+        e2=(EditText) findViewById(R.id.et2);
+        b1=(Button) findViewById(R.id.bt1);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getUser=e1.getText().toString();
+                getPass=e2.getText().toString();
+                if (getUser.equals(s1) && getPass.equals(s2))
+                {
+                    Intent ob1 = new Intent(getApplicationContext(), Dashboard.class);
+                    startActivity(ob1);
+                }
+                else
+                {
+                    Toast.makeText(getApplicationContext(),s3,Toast.LENGTH_LONG).show();
+                }
+            }
+        });
 
     }
 }
